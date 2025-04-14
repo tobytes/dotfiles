005d15b215266332a4080a6dfee6381a9a0361c4
tobytes <mailtobyte@googlemail.com>
2025-04-14 20:30:00 +0200

    [Chore] Formatting

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -1,17 +1,17 @@
 {
-    description = "My NixOS flake";
+  description = "My NixOS flake";
 
-    inputs = {
-        # NixOS official package source, using the nixos-24.11 branch
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs = {
+    # NixOS official package source, using the nixos-24.11 branch
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+  outputs = { self, nixpkgs, ...}@inputs: {
+    nixosConfigurations.toby-thinkpad = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import base configuration
+        ./hosts/toby-thinkpad/configuration.nix
+      ];
     };
-    outputs = { self, nixpkgs, ...}@inputs: {
-        nixosConfigurations.toby-thinkpad = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-                # Import base configuration
-                ./hosts/toby-thinkpad/configuration.nix
-            ];
-        };
-    };
+  };
 }
