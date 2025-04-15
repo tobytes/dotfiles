9cc2abd6da6527d1808cf037cb98d09cf34983d2
tobytes <mailtobyte@googlemail.com>
2025-04-15 23:40:26 +0200

    [IMPL] Hardware specific optimization for thinkpad x230

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -4,11 +4,15 @@
   inputs = {
     # NixOS official package source, using the nixos-24.11 branch
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    # A collection of NixOS modules covering hardware quirks
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
-  outputs = { self, nixpkgs, ...}@inputs: {
+  outputs = { self, nixpkgs, nixos-hardware, ...}@inputs: {
     nixosConfigurations.toby-thinkpad = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        # Hardware config for thinkpad x230 
+        nixos-hardware.nixosModules.lenovo-thinkpad-x230
         # Import base configuration
         ./hosts/toby-thinkpad/configuration.nix
       ];
